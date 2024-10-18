b0d40e89aa29d145baf7d59e527973b2d210d2fa
yuyawk <riotintacos@gmail.com>
2024-10-18 23:45:17 +0900

    modify comment

--- libc_replacer/cc/internal/definition_helper.h
+++ libc_replacer/cc/internal/definition_helper.h
@@ -11,7 +11,7 @@
 /// @param library Name of the libc API to replace.
 /// @param ret_t Return value type of the libc API.
 /// @param ... Argument value types of the libc API.
-/// @note `cppcoreguidelines-avoid-non-const-global-variables` for defining a global atomic function pointer.
+/// @note Disable `cppcoreguidelines-avoid-non-const-global-variables` for defining a global atomic function pointer.
 #define LIBC_REPLACER_INTERNAL_DEFINE(library, ret_t, ...)                                             \
     ret_t __real_##library(LIBC_REPLACER_INTERNAL_GET_ARG_TYPES_AND_NAMES(__VA_ARGS__));               \
     ret_t __wrap_##library(LIBC_REPLACER_INTERNAL_GET_ARG_TYPES_AND_NAMES(__VA_ARGS__));               \
