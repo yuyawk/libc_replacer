2917f95910d73d6ead192216bcde4fbd486dd17a
yuyawk <riotintacos@gmail.com>
2024-10-24 01:28:17 +0900

    clang-tidy

--- examples/cc/malloc/test.c
+++ examples/cc/malloc/test.c
@@ -3,18 +3,24 @@
 #include <assert.h>
 #include <stdlib.h>
 
-static void *mock_malloc(size_t size) { return (void *)size; }
+static void *mock_malloc(size_t size) {
+  // NOLINTNEXTLINE(performance-no-int-to-ptr)
+  return (void *)size;
+}
 
 int main(void) {
   // Check if the API is replaced
   libc_replacer_overwrite_malloc(mock_malloc);
-  const void *got = malloc(123);
-  assert(got == (void *)123);
+  const size_t value_size = 123;
+  const void *got = malloc(value_size);
+  // NOLINTNEXTLINE(clang-analyzer-unix.Malloc,performance-no-int-to-ptr)
+  assert(got == (void *)value_size);
 
   // Check the value after resetting
   libc_replacer_reset_malloc();
-  const void *got_after_reset = malloc(123);
-  assert(got_after_reset != (void *)123);
+  const void *got_after_reset = malloc(value_size);
+  // NOLINTNEXTLINE(clang-analyzer-unix.Malloc,performance-no-int-to-ptr)
+  assert(got_after_reset != (void *)value_size);
 
   return 0;
 }
