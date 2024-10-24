ede05cc6b143d3b30edc8d94b63afd72ce2fbf27
yuyawk <46620009+yuyawk@users.noreply.github.com>
2024-10-25 00:31:17 +0900

    rename directories (#21)

--- development/cli/style_check/clang_tidy_integration_test.bash
+++ development/cli/style_check/clang_tidy_integration_test.bash
@@ -7,6 +7,6 @@ set -euo pipefail
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 source "${SCRIPT_DIR}/../common.bash"
 
-cd "${REPO_ROOT_DIR}/examples"
+cd "${REPO_ROOT_DIR}/integration_test/bazel"
 
 "${BAZEL_EXECUTABLE[@]}" build //... --config clang-tidy
