0923a7f0f4b5a5b28d95c66c623fcb07bedd4257
yuyawk <46620009+yuyawk@users.noreply.github.com>
2024-10-26 02:00:42 +0900

    drop `LIBC_REPLACER_INTERNAL_DEFINE_RETURNING_VOID` (#24)

--- libc_replacer/cc/internal/definition_helper.h
+++ libc_replacer/cc/internal/definition_helper.h
@@ -10,30 +10,7 @@
 /// @param ... Argument value types of the libc API.
 /// @note Disable `cppcoreguidelines-avoid-non-const-global-variables` for
 /// defining a global atomic function pointer.
-#define LIBC_REPLACER_INTERNAL_DEFINE(library, ret_t, ...)                     \
-  LIBC_REPLACER_INTERNAL_DEFINE_IMPLEMENTATION(library, ret_t, return,         \
-                                               __VA_ARGS__)
-
-/// @brief @c LIBC_REPLACER_INTERNAL_DEFINE for the case where @c ret_t is @c
-/// void.
-/// @details In C, @c return keyword shall not be used for
-/// @param library Name of the libc API to replace.
-/// @param ... Argument value types of the libc API.
-/// @note Disable `cppcoreguidelines-avoid-non-const-global-variables` for
-/// defining a global atomic function pointer.
-#define LIBC_REPLACER_INTERNAL_DEFINE_RETURNING_VOID(library, ...)             \
-  LIBC_REPLACER_INTERNAL_DEFINE_IMPLEMENTATION(library, void, , __VA_ARGS__)
-
-/// @brief Implementation of @c LIBC_REPLACER_INTERNAL_DEFINE and @c
-/// LIBC_REPLACER_INTERNAL_DEFINE_RETURNING_VOID
-/// @param library Name of the libc API to replace.
-/// @param ret_t Return value type of the libc API.
-/// @param return_kw @c return if @c ret_t is non-void, empty otherwise
-/// @param ... Argument value types of the libc API.
-/// @note Disable `cppcoreguidelines-avoid-non-const-global-variables` for
-/// defining a global atomic function pointer.
-#define LIBC_REPLACER_INTERNAL_DEFINE_IMPLEMENTATION(library, ret_t,                                                            \
-                                                     return_kw, ...)                                                            \
+#define LIBC_REPLACER_INTERNAL_DEFINE(library, ret_t, ...)                                                                      \
   ret_t __real_##library(                                                                                                       \
       LIBC_REPLACER_INTERNAL_GET_ARG_TYPES_AND_NAMES(__VA_ARGS__));                                                             \
   ret_t __wrap_##library(                                                                                                       \
@@ -51,7 +28,8 @@
       LIBC_REPLACER_INTERNAL_GET_ARG_TYPES_AND_NAMES(__VA_ARGS__)) {                                                            \
     libc_replacer_##library##_func_ptr_t func_got =                                                                             \
         atomic_load(&libc_replacer_func_global);                                                                                \
-    return_kw func_got(LIBC_REPLACER_INTERNAL_GET_ARG_NAMES(__VA_ARGS__));                                                      \
+    LIBC_REPLACER_INTERNAL_RETURN_IF_NOT_VOID(ret_t)                                                                            \
+    func_got(LIBC_REPLACER_INTERNAL_GET_ARG_NAMES(__VA_ARGS__));                                                                \
   }
 
 #endif // INCLUDE_GUARD_LIBC_REPLACER_CC_INTERNAL_DEFINITION_HELPER_H_
