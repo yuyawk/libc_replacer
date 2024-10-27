781cd2c67da66628e3e486f2f390f50fd76950b0
yuyawk <riotintacos@gmail.com>
2024-10-28 03:29:59 +0900

    fix: do not use unsupported macro; instead let the compiler fail

--- libc_replacer/cc/clock_gettime/clock_gettime.c
+++ libc_replacer/cc/clock_gettime/clock_gettime.c
@@ -1,17 +1,17 @@
 #include "libc_replacer/cc/clock_gettime/clock_gettime.h" // IWYU pragma: associated
 #include "libc_replacer/cc/clock_gettime/features.h"
 #include "libc_replacer/cc/internal/definition_helper_syscall.h"
-#include "libc_replacer/cc/internal/definition_helper_unsupported.h" // IWYU pragma: keep
 #include <time.h>
 
 #ifdef LIBC_REPLACER_INTERNAL_HAS_CLOCK_GETTIME
 
 LIBC_REPLACER_INTERNAL_DEFINE_SYSCALL(clock_gettime, int, (clockid_t, clk_id),
                                       (struct timespec *, ts_ptr))
-
 #else
 
-LIBC_REPLACER_INTERNAL_DEFINE_UNSUPPORTED(clock_gettime)
-LIBC_REPLACER_INTERNAL_DEFINE_UNSUPPORTED(__clock_gettime)
+/// @brief Dummy type declaration.
+/// @details Declared because ISO C requires a translation unit to contain at
+/// least one declaration.
+typedef int libc_replacer_internal_dummy_typedef_clock_gettime;
 
 #endif // LIBC_REPLACER_INTERNAL_HAS_CLOCK_GETTIME
