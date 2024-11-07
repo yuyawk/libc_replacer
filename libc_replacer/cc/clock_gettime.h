eb545cef424084523c4d70699b751955facb3b53
yuyawk <riotintacos@gmail.com>
2024-11-08 02:15:51 +0900

    forgotten to commit

---
+++ libc_replacer/cc/clock_gettime.h
@@ -0,0 +1,7 @@
+
+#ifndef INCLUDE_GUARD_LIBC_REPLACER_CC_CLOCK_GETTIME_H_
+#define INCLUDE_GUARD_LIBC_REPLACER_CC_CLOCK_GETTIME_H_
+
+#include "libc_replacer/cc/clock_gettime/clock_gettime.h" // IWYU pragma: export
+
+#endif // INCLUDE_GUARD_LIBC_REPLACER_CC_CLOCK_GETTIME_H_
